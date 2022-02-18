0826798ab065d6761d7123abfd19027669ece22e
Olena <72007221+Oleva-abr@users.noreply.github.com>
2022-02-18 11:46:41 +0200

    finish dashboard

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -1,6 +1,8 @@
+import { Board } from './../../models/board.models';
 import { Component, OnInit } from '@angular/core';
 
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { Column } from 'src/app/models/column.model';
 
 @Component({
   selector: 'app-main-view',
@@ -10,6 +12,32 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
 export class MainViewComponent implements OnInit {
 
   constructor() { }
+
+  board: Board = new Board('Test Board', [
+    new Column('Ideas', [
+      "Some random idea",
+      "This is another random idea",
+      "build an awesome application"
+    ]),
+    new Column('Research', [
+      "Lorem ipsum",
+      "foo",
+      "This was in the 'Research' column"
+    ]),
+    new Column('Todo', [
+      'Get to work',
+      'Pick up groceries',
+      'Go home',
+      'Fall asleep'
+    ]),
+    new Column('Done', [
+      'Get up',
+      'Brush teeth',
+      'Take a shower',
+      'Check e-mail',
+      'Walk dog'
+    ])
+  ]);
 
   ngOnInit(): void {
     
